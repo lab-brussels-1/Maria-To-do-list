95aaf2477b628b63ada36e91d44bad91d7e78a78
Maria <naplesaries2014@gmail.com>
2022-09-05 13:19:58 +0200

    displayli

--- src/components/displayLi.js
+++ src/components/displayLi.js
@@ -2,13 +2,15 @@ import { TODO_CLASS } from "../data.js";
 const todoList = document.querySelector(TODO_CLASS);
 const numToDo = document.getElementById("num");
 export const displayList = (list) => {
-  var toDocount= Object.keys(list).length;
-  numToDo.innerHTML = `You have ${toDocount} pending tasks`;
+  var toDocount = Object.keys(list).length;
   let newLitag = "";
-  list.forEach((element) => {
-    newLitag =
-      `<li> ${element.title}<span onclick="deleteTodo(${element.id})"; > - </span></li>` +
-      newLitag;
-  });
+  if (toDocount !== 0) {
+    list.forEach((element) => {
+      newLitag =
+        `<li> ${element.title}<span onclick="deleteTodo(${element.id})"; > - </span></li>` +
+        newLitag;
+    });
+  }
+  numToDo.innerHTML = `You have ${toDocount} pending tasks`;
   todoList.innerHTML = newLitag;
 };
